b3d03dfd02a25748b6876e68fe91cf0f0ff8de93
Philipp Kats <casyfill@gmail.com>
2019-06-23 17:28:57 -0400

    serving model

---
+++ Chapter17/hello_world.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+db = {'noise': 24,
+      'broken hydrant': 2}
+
+@app.get("/complaints/{complaint_type}")
+def complaints(complaint_type: str, hour:int):
+    return {"complaint_type": complaint_type, 
+            "hour": hour,
+            "q": db.get(complaint_type, None)}
