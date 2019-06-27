f10f669a8063c1dc2a8cfbc365cb767a3aeb6005
Philipp Kats <casyfill@gmail.com>
2019-06-26 21:37:01 -0400

    numpy version - need to show conversion

--- Chapter18/311predictions-v2/app.py
+++ Chapter18/311predictions-v2/app.py
@@ -1,23 +1,29 @@
 import pickle
 
 import boto3
-import pandas as pd
+import numpy as np
 from chalice import Chalice, Response
-from sklearn.base import BaseEstimator
-from ml import TimeTransformer
+from datetime import datetime
 
-S3 = boto3.client('s3', region_name='us-east-1')
+from ml import TimeTransformer
 BUCKET, KEY = 'philipp-packt', 'model.pkl'
 
-response = S3.get_object(Bucket=BUCKET, Key=KEY)
-body = response['Body'].read()
-model = pickle.loads(body)
+
+def _load_pickle(bucket, key):
+    S3 = boto3.client('s3', region_name='us-east-1')
+    response = S3.get_object(Bucket=bucket, Key=key)
+    
+    body = response['Body'].read()
+    return pickle.loads(body)
+
+model = _load_pickle(BUCKET, KEY)
 
 app = Chalice(app_name='311predictions-v2')
-singleton = pd.DataFrame([{'complaint_type':'dummy', 
-                           'latitude':1.1111, 
-                           'longitude':1.1111,
-                           'created_date':pd.to_datetime('2019-01-01')}])
+# singleton = pd.DataFrame([{'complaint_type':'dummy', 
+#                            'latitude':1.1111, 
+#                            'longitude':1.1111,
+#                            'created_date':pd.to_datetime('2019-01-01')}])
+singleton = np.empty(shape=(1, 4), dtype='object')
 
 mapping = {
     'lon': 'longitude',
@@ -28,19 +34,21 @@ mapping = {
 dtypes = {
     'lon': float,
     'lat': float,
-    'date': pd.to_datetime
+    'date': np.datetime64
 }
 
 @app.route('/predict/{complaint_type}', methods=['GET'])
 def index(complaint_type:str):
-    singleton.loc[0, 'complaint_type'] = complaint_type
+    app.log.debug(app.current_request.query_params)
+    singleton[0, 0] = complaint_type
 
-    for k, col in mapping.items():
-        singleton.loc[0, col] = dtypes[k](app.current_request.query_params.get(k, pd.np.nan))
+    for i, col in enumerate(mapping.keys(), 1):
+        singleton[0, i] = dtypes[col](app.current_request.query_params.get(col, np.nan))
 
+    app.log.debug(singleton.astype(str).tolist())
     try:
-        prediction = model.predict(singleton[['complaint_type', 'latitude', 'longitude','created_date']])[0]
-
+        prediction = model.predict(singleton)[0]
+        app.log.debug(prediction)
         return Response(status_code=200,
                         headers={'Content-Type': 'application/json'},
                         body={'status': 'success',
@@ -49,6 +57,4 @@ def index(complaint_type:str):
         return Response(status_code=400,
                         headers={'Content-Type': 'application/json'},
                         body={'status': 'failure',
-                              'features': singleton.astype(str).to_dict(orient='records')[0],
-                              'inbound': app.current_request.query_params,
                               'error message': str(e)})
