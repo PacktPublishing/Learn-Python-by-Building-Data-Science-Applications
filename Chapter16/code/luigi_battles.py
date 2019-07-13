73fe058e70ab153d55193e4a285bd6cbd3786cea
Philipp Kats <casyfill@gmail.com>
2019-07-12 20:03:28 -0400

    wwiii luigi

--- Chapter16/code/luigi_battles.py
+++ Chapter16/code/luigi_battles.py
@@ -11,30 +11,17 @@ from wikiwwii.collect.battles import parse_battle_page
 folder = Path(__file__).parents[1] / 'data' 
 
 
-class ParseBattle(luigi.Task):
-    url = luigi.Parameter()
-    front = luigi.Parameter(default='undefined')
-    
-    def output(self):
-        name = self.url.split('/')[-1][:-5]
-        path = str(folder / 'fronts' / f'{self.front}/{name}.json')
-        return luigi.LocalTarget(path)
-    
-    def run(self):
-        result = parse_battle_page(self.url, )
-
-        with open(self.output().path, 'w') as f:
-            json.dump(result, f)
 
 
 class ParseFront(luigi.Task):
     front = luigi.Parameter()
 
     def requires(self):
-        return [ParseBattle(front=self.front, url=)]
+        return ParseFronts()
      
     def output(self):
-        return 
+        path = str(folder / 'fronts' / f'{self.front}.json')
+        return luigi.LocalTarget(path)
  
     def run(self):
         with open(self.input().path, 'r') as f:
@@ -46,6 +33,7 @@ class ParseFront(luigi.Task):
         for cp_name, campaign in front.items():
             result[cp_name] = _parse_in_depth(campaign, cp_name)
 
+        self.output().makedirs()
         with open(self.output().path, 'w') as f:
             json.dump(result, f)
 
