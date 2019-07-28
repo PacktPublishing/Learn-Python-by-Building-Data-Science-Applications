f6db9a02120cb23f375c5baa6b86ec5cf09d349f
Philipp Kats <casyfill@gmail.com>
2019-07-28 13:55:26 -0400

    added 311 script for chapter 6

---
+++ Chapter06/collect_311.py
@@ -0,0 +1,42 @@
+import requests as rq
+from datetime import date
+import json
+
+NYCOD = 'YOUR API KEY HERE'
+folder = '.'
+
+time_col = "Created Date"
+resource = "fhrw-4uyv"
+
+def _get_data(resource:str, time_col:str, date:date, offset:int=0):
+    """collect data from NYC open data
+
+    Args:
+        resource: NYC OD resource id
+        time_col: column that stores timestampes to query on
+        date: date to pull data for
+        offset: records offset. You probably want to keep it equal to zero.
+    """
+
+    Q = f"where=created_date between '{date}' AND '{date}T23:59:59.000'"
+    url = f"https://data.cityofnewyork.us/resource/{resource}.json?$limit=50000&$offset={offset}&${Q}"
+
+    headers = {"X-App-Token": NYCOD} if NYCOD else None
+    r = rq.get(url, headers=headers)
+    r.raise_for_status()
+
+    data = r.json()
+    if len(data) == 50_000:
+        offset2 = offset + 50000
+        data2 = _get_data(resource, time_col, date, offset=offset2)
+        data.extend(data2)
+
+    return data
+
+
+if __name__ == '__main__':
+    today = date.today()
+    data = _get_data(resource, time_col, date=today)
+
+    with open(f'311_{today:%Y-%m-%d}.json', 'w') as f:
+        json.dump(data, f)
