ffb527d83183afda5cea66f7a822b13d7802dada
Philipp Kats <casyfill@gmail.com>
2019-07-29 15:26:54 -0400

    chapter 8 refactoring

--- Chapter08/animals.py
+++ Chapter08/animals.py
@@ -1,15 +1,10 @@
 import random
-from statistics import mean, stdev
+from statistics import mean
 
-
-class Animal:
-    __slots__ = [
-        "age",
-        "survival_skill",
-        "fertility_rate",
-        "mutatlion_drift",
-        "max_age",
-    ]
+class Herbivore:
+    mutation_drift = 1
+    fertility_rate = 0.25
+    max_age = 4
 
     def __init__(self, survival_skill):
         self.age = 0
@@ -19,78 +14,67 @@ class Animal:
         self.age += 1
 
     def breed(self):
-        mutation = self.survival_skill + random.randint(
-            -1 * self.mutation_drift, self.mutation_drift
-        )
+        drift = random.randint(-1 * self.mutation_drift, self.mutation_drift)
+        mutation = self.survival_skill + drift
+
         return self.__class__(survival_skill=mutation)
 
 
-class Rabbit(Animal):
-    """Herbivour Animal"""
-
-    __slots__ = ["age", "survival_skill", "fertility_rate", "mutation_drift", "max_age"]
-
-    def __init__(self, survival_skill):
-        self.mutation_drift = 1
-        self.fertility_rate = 0.25  # serious undermined
-        self.max_age = 4
-        super().__init__(survival_skill)
-
-
 class Island:
-    __slots__ = ["stats", "rabbits", "environment", "max_pop", "year"]
+    max_pop, year = 0, 0
 
-    def __init__(self, init_rabbits=10, max_pop=2500):
-        self.stats = dict()
-        self.max_pop = max_pop
+    def __init__(self, initial_pop=10, max_pop=2500):
         self.year = 0
-        self.rabbits = [
-            Rabbit(survival_skill=random.randint(0, 100)) for _ in range(init_rabbits)
+        self.max_pop = max_pop
+        self.stats = dict()
+
+        self.animals = [
+            Herbivore(survival_skill=random.randint(0, 100))
+            for _ in range(initial_pop)
         ]
 
-    def _compute_rabbits(self):
-        """"""
-        new_rabbits = list()
-        pop = len(self.rabbits)
+    def _simulate(self):
+        new_animals = list()
+        pop = len(self.animals)
 
-        for r in self.rabbits:
-            # for now all herbivores survive
-            exceed_pop = pop > self.max_pop
-            if not exceed_pop and random.random() <= r.fertility_rate:
-                new_rabbits.append(r.breed())
-                pop += 1
+        random.shuffle(self.animals)
+        for animal in self.animals:
+            # step 1. breeding
+            if pop <= self.max_pop:
+                if random.random() <= animal.fertility_rate:
+                    new_animals.append(animal.breed())
+                    pop += 1
 
-            r._age()
-            if r.age <= r.max_age:  # old ones die
-                new_rabbits.append(r)
+            # step 2. Aging and dying
+            animal._age()
+            if animal.age <= animal.max_age:  # dies of age
+                new_animals.append(animal)
 
-        self.rabbits = new_rabbits
+        self.animals = new_animals
 
     def _collect_stats(self):
-        """run statistics. 
-        It would be more efficient to collect them in the "compute" loop, 
-        but this way it is more readable"""
-        year_stats = {"pop": len(self.rabbits)}
+        """run island statistics"""
 
-        if len(self.rabbits) > 0:
-            ages, skills = zip(*[(h.age, h.survival_skill) for h in self.rabbits])
+        year_stats = {"pop": len(self.animals)}
+
+        if len(self.animals) > 0:
+            ages, skills, ss_75 = zip(
+                *[
+                    (a.age, a.survival_skill, (a.survival_skill > 75))
+                    for a in self.animals
+                ]
+            )
             year_stats["mean_age"] = mean(ages)
             year_stats["mean_skill"] = mean(skills)
-            year_stats["75_skill"] = (
-                len([r for r in self.rabbits if r.survival_skill > 75])
-                / year_stats["pop"]
-            )
+            year_stats["75_skill"] = sum(ss_75) / year_stats["pop"]
 
         self.stats[self.year] = year_stats
 
-    def epoch(self):
-        self._compute_rabbits()
-        self._collect_stats()
-        self.year += 1
-
-    def compute_epoches(self, N):
-        for _ in range(N):
-            self.epoch()
+    def compute_epoches(self, years):
+        for _ in range(years):
+            self._simulate()
+            self._collect_stats()
+            self.year += 1
 
         return self.stats
 
@@ -101,16 +85,14 @@ class HarshIsland(Island):
     Rabbits with survival skill below the condition die at the beginning of the epoch
     """
 
-    __slots__ = ["stats", "rabbits", "environment", "max_pop", "year", "env_range"]
-
     def __init__(self, env_range, **kwargs):
         self.env_range = env_range
         super().__init__(**kwargs)
 
     def _compute_env(self):
         condition = random.randint(*self.env_range)
-        self.rabbits = [r for r in self.rabbits if r.survival_skill >= condition]
+        self.animals = [a for a in self.animals if a.survival_skill >= condition]
 
-    def _compute_rabbits(self):
+    def _simulate(self):
         self._compute_env()
-        super()._compute_rabbits()
+        super()._simulate()
